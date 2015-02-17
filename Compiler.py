4891fd6323419594a67155ca3f3cdf6a9026c008
kotkarl <kotkarl@gmail.com>
2015-02-17 15:34:57 +0000

    Compiler finished and Parser fiddling

--- Compiler.py
+++ Compiler.py
@@ -1,1 +1,8 @@
+from Lexer import Lexer
+from Parser import Parser
+
 __author__ = 'Hrafnkell'
+
+myLexer = Lexer()
+myParser = Parser(myLexer)
+myParser.parse()
--- Parser.py
+++ Parser.py
@@ -1,1 +1,17 @@
+from Lexer import Lexer
+from Token import Token
+
 __author__ = 'Hrafnkell'
+
+
+class Parser(object):
+    lexer = Lexer()
+
+    def __init__(self):
+        self.token = Token()
+
+    def parse(self):
+        pass
+
+
+
