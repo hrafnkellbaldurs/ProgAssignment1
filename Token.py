df3b08327acbaad5b0eb6f1cd34d94a16922e06f
kotkarl <kotkarl@gmail.com>
2015-02-17 11:18:16 +0000

    Finished Token.py class

--- Token.py
+++ Token.py
@@ -3,8 +3,12 @@ __author__ = 'Hrafnkell'
 
 class Token(object):
     def __init__(self, lexeme, tCode):
+        TokenCode = ["ID", "ASSIGN", "SEMICOL", "INT", "PLUS", "MINUS",
+                     "MULT", "LPAREN", "RPAREN", "PRINT", "END", "ERROR"]
         self.lexeme = lexeme
-        self.tCode = tCode
+        self.tCode = TokenCode[tCode]
+
+
 
 
 
