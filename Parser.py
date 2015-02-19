47d83c1927a1f6d613d30aba21bebbcc26e5824e
kotkarl <kotkarl@gmail.com>
2015-02-19 01:39:23 +0000

    parser and lexer done

--- Parser.py
+++ Parser.py
@@ -13,7 +13,7 @@ class Parser(object):
     ''' Constructor '''
     def __init__(self, lexer):
         self.lexer = lexer
-        self.nextToken = Token()
+        self.nextToken = Token("+", "+")
 
     ''' Goes through input and diagnoses syntax '''
     def parse(self):
@@ -22,71 +22,86 @@ class Parser(object):
 
     ''' Statements -> Statement ; Statements | end '''
     def statements(self):
-        if self.nextToken == "END":
-            self.nextToken == self.lexer.nextToken()
+        if self.nextToken.tCode == "END":
+            #self.nextToken == self.lexer.nextToken()
             sys.exit(1)
         else:
             self.statement()
-            if self.nextToken == "SEMICOL":
-                self.nextToken == self.lexer.nextToken()
+            if self.nextToken.tCode == "SEMICOL":
+                self.nextToken = self.lexer.nextToken()
                 self.statements()
+        if(self.nextToken.tCode == "ERROR"):
+            self.error()
 
     ''' Statement -> id = Expr | print id '''
     def statement(self):
-        if self.nextToken == "ID":
-            self.nextToken == self.lexer.nextToken()
-            if self.nextToken == "ASSIGN":
-                self.nextToken == self.lexer.nextToken()
+        if self.nextToken.tCode == "ID":
+            print("PUSH " + self.nextToken.lexeme)
+            self.nextToken = self.lexer.nextToken()
+            if self.nextToken.tCode == "ASSIGN":
+                self.nextToken = self.lexer.nextToken()
                 self.expr()
+                print("ASSIGN")
             else:
                 self.error()
-        elif self.nextToken == "PRINT":
-            self.nextToken == self.lexer.nextToken()
-            if self.nextToken == "ID":
-                self.nextToken == self.lexer.nextToken()
-                #Skrifa ut assembly
+        elif self.nextToken.tCode == "PRINT":
+            self.nextToken = self.lexer.nextToken()
+            #print("PRINT")
+            if self.nextToken.tCode == "ID":
+                print("PUSH " + self.nextToken.lexeme)
+                print("PRINT")
+                self.nextToken = self.lexer.nextToken()
             else:
                 self.error()
         else:
+            self.error()
+        if(self.nextToken.tCode == "ERROR"):
             self.error()
 
 
     ''' Expr -> Term | Term + Expr | Term - Expr '''
     def expr(self):
         self.term()
-        if self.nextToken == "PLUS":
-            self.nextToken == self.lexer.nextToken()
+        if self.nextToken.tCode == "PLUS":
+            self.nextToken = self.lexer.nextToken()
             self.expr()
-        elif self.nextToken == "MINUS":
-            self.nextToken == self.lexer.nextToken()
+            print("ADD")
+        elif self.nextToken.tCode == "MINUS":
+            self.nextToken = self.lexer.nextToken()
             self.expr()
-        else:
+            print("SUB")
+        if(self.nextToken.tCode == "ERROR"):
             self.error()
 
     ''' Term -> Factor | Factor * Term '''
     def term(self):
         self.factor()
-        if self.nextToken == "MULT":
-            self.nextToken == self.lexer.nextToken()
+        if self.nextToken.tCode == "MULT":
+            self.nextToken = self.lexer.nextToken()
             self.term()
-        else:
+            print("MULT")
+        if(self.nextToken.tCode == "ERROR"):
             self.error()
 
 
     ''' Factor -> int | id | (Expr) '''
     def factor(self):
-        if self.nextToken == "INT":
+        if self.nextToken.tCode == "INT":
+            print("PUSH " + self.nextToken.lexeme)
             self.nextToken = self.lexer.nextToken()
-        elif self.nextToken == "ID":
-            self.nextToken == self.lexer.nextToken()
-        elif self.nextToken == "LPAREN":
-            self.nextToken == self.lexer.nextToken()
+        elif self.nextToken.tCode == "ID":
+            print("PUSH " + self.nextToken.lexeme)
+            self.nextToken = self.lexer.nextToken()
+        elif self.nextToken.tCode == "LPAREN":
+            self.nextToken = self.lexer.nextToken()
             self.expr()
-            if self.nextToken == "RPAREN":
-                self.nextToken == self.lexer.nextToken()
+            if self.nextToken.tCode == "RPAREN":
+                self.nextToken = self.lexer.nextToken()
             else:
                 self.error()
         else:
+            self.error()
+        if(self.nextToken.tCode == "ERROR"):
             self.error()
 
 
